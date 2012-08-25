dc643f651b0def3b35ea7a0bd940a8ad46edaa5b
mizoryu <suzunatsu@yahoo.com>
2012-08-25 09:46:34 +0900

    facebook

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -10,13 +10,25 @@ class AuthenticationsController < UserBaseController
   end
 
   def callback
-    facebook = Settings.web_services.facebook
-    fbauth = FbGraph::Auth.new(facebook.access_token, facebook.access_secret)
-    fbauth.client.redirect_uri = facebook.redirect_uri
     fbauth.client.authorization_code = params[:code]
     access_token = fbauth.client.access_token!(:client_auth_body)
     me = FbGraph::User.me(access_token).fetch
     current_user.authentications.create(service_type: 'facebook', access_token: access_token.to_s, uid: me.identifier)
     redirect_to authentications_path
+  end
+
+  def destroy
+    authentication = current_user.authentications.find(params[:id])
+    authentication.destroy
+    redirect_to :back
+  end
+
+  private
+  def fbauth
+    return @fbauth if @fbauth
+    facebook = Settings.web_services.facebook
+    @fbauth = FbGraph::Auth.new(facebook.access_token, facebook.access_secret)
+    @fbauth.client.redirect_uri = facebook.redirect_uri
+    @fbauth
   end
 end
--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,7 @@
 class Authentication < ActiveRecord::Base
   attr_accessible :access_token, :expires_at, :service_type, :uid
+
+  def profile_icon_url
+    "https://graph.facebook.com/#{self.uid}/picture"
+  end
 end
