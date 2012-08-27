2ee626d40cbb8f4f178b6d8c90b68896bfe44a2b
mizoR <suzunatsu@yahoo.com>
2012-08-27 18:50:22 +0900

    TwitterにもPOSTできるようにした

--- script/post_to_sns.rb
+++ script/post_to_sns.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+require 'open-uri'
+
 MAX_TRIED_TIMES = 3
 
 reserves = Reserve.where('posted_at IS NULL AND posts_at <= ? AND tried_times < ?', Time.now, MAX_TRIED_TIMES)
@@ -20,6 +22,30 @@ reserves.each do |reserve|
       reserve.update_attributes(posted_at: Time.now, tried_times: reserve.tried_times + 1)
     end
   when 'twitter'
+    twitter = Settings.web_services.twitter
+    Twitter.configure do |config|
+      config.consumer_key       = twitter.access_token
+      config.consumer_secret    = twitter.access_secret
+      config.oauth_token        = authentication.access_token
+      config.oauth_token_secret = authentication.access_secret
+    end
+    tweet = "#{feed.message}\n#{feed.link}"
+    if feed.picture.present?
+      uri = URI.parse(feed.picture)
+      uri.open { |_media|
+        # NOTE 直接_media(TempFileインスタンス)を渡すと、
+        # update_with_mediaでエラー(Method missing original_filename)
+        # が発生したので、暫定的にFileインスタンスに置き換える
+        media = File.open(_media.path)
+        if Twitter.update_with_media(tweet, media)
+          reserve.update_attributes(posted_at: Time.now, tried_times: reserve.tried_times + 1)
+        end
+      }
+    else
+      if Twitter.update(tweet)
+        reserve.update_attributes(posted_at: Time.now, tried_times: reserve.tried_times + 1)
+      end
+    end
   end
 end
 
