2f6ef698dd24af20e772ef5a164d196c78d55d85
mizoR <suzunatsu@yahoo.com>
2012-08-30 09:00:26 +0900

    user associations

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,11 +1,10 @@
 class FeedsController < UserBaseController
 
   def index
-    @feeds = Feed.all
   end
 
   def show
-    @feed = Feed.find(params[:id])
+    @feed = current_user.feeds.find(params[:id])
   end
 
   def new
@@ -29,7 +28,7 @@ class FeedsController < UserBaseController
   end
 
   def edit
-    @feed = Feed.find(params[:id])
+    @feed = current_user.feeds.find(params[:id])
     current_user.authentications.each do |authentication|
       if !@feed.authentications.include?(authentication)
         @feed.reserves.build(authentication_id: authentication.id)
@@ -48,7 +47,7 @@ class FeedsController < UserBaseController
   end
 
   def destroy
-    feed = Feed.find(params[:id])
+    feed = current_user.feeds.find(params[:id])
     feed.destroy
     redirect_to feeds_path
   end
